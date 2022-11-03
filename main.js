84a0916ffb24169826ed2e5cc3433ba9477ca9d0
JohnWesley14 <johnwesleyrvaz@gmail.com>
2022-11-03 16:19:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+function createGame(player1, hour, player2){
+    return `
+    <li>
+        <img src="assets/${player1}.svg" alt="Bandeira do ${player1}">
+        <strong>16:00</strong>
+        <img src="assets/${player2}.svg" alt="Bandeira da ${player2}">
+     </li>
+     
+    `
+}
+function createCard(date, day, games){
+ return `
+    <div class="card">
+        <h2>${date}<span>${day}</span></h2>
+
+        <ul>
+         ${games}
+        </ul>
+    </div>
+` 
+}
+
+document.querySelector("#app").innerHTML = `
+<header>
+    <img src="./assets/logo.svg" alt="Logo da NLW">
+</header>
+<main id="cards">
+  ${createCard("24/11", "quinta",
+   createGame(
+    "brazil", 
+    "16:00", 
+    "serbia")
+   )}
+   ${createCard("28/11", "Segunda",
+   createGame(
+    "brazil", 
+    "16:00", 
+    "switzerland")
+   )}
+   ${createCard("24/11", "Segunda",
+   createGame(
+    "brazil", 
+    "16:00", 
+    "cameroon")
+   )}
+</main>
+`;
